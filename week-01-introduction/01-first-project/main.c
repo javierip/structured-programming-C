ddf8119aeab1c2d60d1ecb8d8b52590d6b443c33
Javier Iparraguirre <jiparraguirre@gmail.com>
2023-03-20 13:20:34 -0300

    Initial Code::Blocks project

---
+++ week-01-introduction/01-first-project/main.c
@@ -0,0 +1,13 @@
+/* Program: Hello UTN
+ * Author: Javier Iparraguirre
+ * E-mail: jiparraguirre@frbb.utn.edu
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hola UTN!\n");
+    return 0;
+}
